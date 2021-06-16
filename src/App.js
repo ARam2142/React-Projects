2eb6da1576038c82a13be73f0269b1fd403346b5
Aram2142 <andresaaramirez@aol.com>
2021-06-15 23:56:49 -0400

    small excercises

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="background">
+      <button>Press Me</button>
+    </div>
+  );
+}
+
+export default App;
